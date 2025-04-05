98d0d34b426ba46819fbd16bc56d5766cc7d2cd6
Jeremy <jeremywawesh10@gmail.com>
2025-04-05 11:42:40 +0300

    Worked on my script.js file

--- script.js
+++ script.js
@@ -37,6 +37,55 @@ function loadTransactionsFromCookies() {
       updateUI();//refreshes the screen to show updated list of transactions
     }
   });
+
+  //Remove transaction
+  function removeTransaction(id) {
+    transactions = transactions.filter(t => t.id !== id);//keeps all transactions except the one with the given id
+    updateUI();
+  }
+
+  //Filter transactions
+  function filterTransactions(type) {//it filters transactions based on a certain type
+    updateUI(type);
+  }
+
+  //Update UI
+  function updateUI(filter = 'all') {
+    transactionList.innerHTML = '';//clears any previous list
+    let income = 0;//variables for total
+    let expense = 0;//variables for total
+  
+    transactions.forEach((t) => {//loops through all the transactions
+      if (filter !== 'all' && (filter === 'income' && t.amount < 0 || filter === 'expense' && t.amount > 0)) {
+        return;
+      }
+  
+      const li = document.createElement('li');//creates a new list item which represents a single transaction
+      li.className = t.amount >= 0 ? 'income' : 'expense';
+      li.innerHTML = `
+        ${t.desc} - Ksh ${Math.abs(t.amount)}
+        <button onclick="removeTransaction(${t.id})">❌</button> 
+      `;
+      transactionList.appendChild(li);//adds the newly created list
+  
+      if (t.amount >= 0) {
+        income += t.amount;
+      } else {
+        expense += t.amount;
+      }
+    });
+  
+    //Update the UI with totals
+    totalIncomeEl.textContent = `Ksh ${income}`;
+    totalExpenseEl.textContent = `Ksh ${Math.abs(expense)}`;//updates by showing a positive number using Math.abs()
+    balanceEl.textContent = `Ksh ${income + expense}`;
+    saveTransactionsToCookies();//saves transactions to cookies
+  }
+  
+// Initialize UI
+updateUI();
+
+  
   
 
   
